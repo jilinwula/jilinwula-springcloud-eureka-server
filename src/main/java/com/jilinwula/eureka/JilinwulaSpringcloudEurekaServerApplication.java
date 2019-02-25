1757a035e27f690431af821c7ccaaf507a596dad
吉林乌拉 <admin@jilinwula.com>
2019-02-25 17:59:24 +0800

    Eureka注册中心服务端

---
+++ src/main/java/com/jilinwula/eureka/JilinwulaSpringcloudEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jilinwula.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JilinwulaSpringcloudEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JilinwulaSpringcloudEurekaServerApplication.class, args);
+    }
+
+}
